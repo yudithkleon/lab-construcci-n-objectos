ec308cd49f717111c3c80c0a647fe4c508b015d9
yudithleon <yudithkleon@gmail.com>
2025-11-05 14:31:52 -0500

    entrega de ejercicios de Objetos

---
+++ ejercicio1.js
@@ -0,0 +1,19 @@
+//Libro
+function Libro(titulo, autor){
+    this.titulo = titulo;
+    this.autor = autor;
+    this.prestado = false;
+
+    this.prestar = function(){
+        if(!this.prestado){
+            this.prestado = true;
+            console.log(`El libro "${this.titulo}" ha sido prestado.`);
+        } else {
+            console.log(`El libro "${this.titulo}" ya está prestado, No disponible.`);
+        }
+    }
+}
+
+const libro1 = new Libro("El poder del Ahora", "Eckhart Tolle");
+libro1.prestar();
+libro1.prestar(); 
